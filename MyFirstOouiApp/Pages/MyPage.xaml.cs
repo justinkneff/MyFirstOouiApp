24125d4c7dfb941754fbb39eff12f6c97e9f1eff
Justin Neff <justin.neff@inclusa.org>
2018-01-08 15:41:38 -0600

    First commit

---
+++ MyFirstOouiApp/Pages/MyPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyFirstOouiApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var button = sender as Button;
+            if (button == null)
+                return;
+            count++;
+            button.Text = $"You clicked {count} times!";
+        }
+    }
+}
